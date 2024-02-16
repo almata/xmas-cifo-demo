6a8bbab2108f8247dd9446cb55f5167508bd599b
Albert Mata <hello@albertmata.net>
2024-02-16 09:39:31 +0100

    Fix typo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = ({ totalPrice, remainingPrice }) => {
   return (
     <header>
-      <h1>Xmas gift ideas</h1>
+      <h1>Birthdays gift ideas</h1>
       <h2>
         Total budget: {totalPrice} € – Remaining to spend: {remainingPrice} €
       </h2>
